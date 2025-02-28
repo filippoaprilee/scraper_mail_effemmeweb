9be72a537f785914d9f27c179d7f0dd5e74a6fcc
filippoaprilee <filippoaprilee@gmail.com>
2025-02-28 16:36:45 +0100

    123

--- helper/estrai_comuni/estrai.py
+++ helper/estrai_comuni/estrai.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL della pagina Wikipedia
-url = "https://it.wikipedia.org/wiki/Comuni_della_Campania"
+url = "https://it.wikipedia.org/wiki/Comuni_della_Sardegna"
 
 response = requests.get(url)
 response.raise_for_status()  # Controlla eventuali errori
